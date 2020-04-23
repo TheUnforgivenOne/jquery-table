fc6f42bf1a87243538f2ba94487542070e96729d
Vladislav <vladpotapow@yandex.ru>
2020-04-23 23:23:01 +0300

    Added basic script

---
+++ src/main.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {name: 'Product1', count: 1, Price: 100},
+        {name: 'Product2', count: 2, Price: 200},
+        {name: 'Product2', count: 3, Price: 300},
+    ],
+    orderBy: name,
+    orderOrder: DESC,
+};
+
+const renderTableHead = () => {
+
+};
+
+const renderTableBody = () => {
+
+};
+
+const render = () => {
+    renderTableHead();
+    renderTableBody();
+};
+
+render();
