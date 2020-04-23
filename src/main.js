8de9150ebfbaacd62b8a6735b0279b288d6e765d
Vladislav <vladpotapow@yandex.ru>
2020-04-24 00:39:26 +0300

    Functions for render table

--- src/main.js
+++ src/main.js
@@ -1,19 +1,37 @@
 const data = {
     products: [
-        {name: 'Product1', count: 1, Price: 100},
-        {name: 'Product2', count: 2, Price: 200},
-        {name: 'Product2', count: 3, Price: 300},
+        {name: 'Product1', count: 1, price: 100},
+        {name: 'Product2', count: 2, price: 200},
+        {name: 'Product2', count: 3, price: 300},
     ],
-    orderBy: name,
-    orderOrder: DESC,
+    sortBy: 'name',
+    sortOrder: 'DESC',
 };
 
 const renderTableHead = () => {
-
+    const tableHead = $('#tableHead');
+    
+    $(tableHead).append(
+        $('<tr>'),
+        $($('<th>').text('Name')),
+        $($('<th>').text('Count')),
+        $($('<th>').text('Price'))
+    );
 };
 
 const renderTableBody = () => {
+    const products = data.products;
+    const tableBody = $('#tableBody');
 
+    products.forEach((product) => (
+            $(tableBody).append(
+                $('<tr>'),
+                $('<td>').text(product.name),
+                $('<td>').text(Number(product.count)),
+                $('<td>').text(Number(product.price))
+            )
+        )
+    )
 };
 
 const render = () => {
