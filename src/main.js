8808127d7d37c04345a5cc9fe7f38768ef7c02c4
Vladislav <vladpotapow@yandex.ru>
2020-04-24 22:33:41 +0300

    Added sorting functional

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 const data = {
     products: [
         {name: 'Product1', count: 1, price: 100},
-        {name: 'Product2', count: 2, price: 200},
-        {name: 'Product2', count: 3, price: 300},
+        {name: 'Product2', count: 18, price: 1200},
+        {name: 'Product3', count: 5, price: 300},
     ],
     sortBy: 'name',
-    sortOrder: 'DESC',
+    sortOrder: 'desc',
 };
 
 const editHandler = () => {
@@ -16,22 +16,57 @@ const deleteHandler = () => {
 
 };
 
+const toggleSortBy = () => {
+    data.sortBy = data.sortBy === 'name' ? 'price' : 'name';
+};
+
+const toggleSortOrder = () => {
+    data.sortOrder = data.sortOrder === 'desc' ? 'asc' : 'desc';
+};
+
+const sortHandler = (event) => {
+    if (data.sortBy === event.data.column) {
+        toggleSortOrder();
+    } else {
+        data.sortOrder = 'asc';
+        toggleSortBy();
+    }
+    render();
+};
+
 const renderTableHead = () => {
     const tableHead = $('#tableHead');
 
+    const nameCol = $('<th>').append(
+        $('<a>')
+            .attr('href', '#')
+            .text('Name')
+            .click({column: 'name'}, sortHandler)
+    );
+    const priceCol = $('<th>').append(
+        $('<a>')
+            .attr('href', '#')
+            .text('Price')
+            .click({column: 'price'}, sortHandler)
+    );
+    const actionsCol = $('<th>').text('Actions');
+
+    $(tableHead).empty();
     $(tableHead).append(
         $('<tr>'),
-        $('<th>').text('Name'),
-        $('<th>').text('Price'),
-        $('<th>').text('Actions')
+        $(nameCol),
+        $(priceCol),
+        $(actionsCol)
     );
 };
 
 const renderTableBody = () => {
-    const products = data.products;
     const tableBody = $('#tableBody');
+    const {products, sortBy, sortOrder} = data;
+    const sortedProducts = _.orderBy(products, sortBy, sortOrder);
 
-    products.forEach((product) => (
+    $(tableBody).empty();
+    sortedProducts.forEach((product) => (
             $(tableBody).append(
                 $('<tr>'),
                 $('<td>').text(product.name),
